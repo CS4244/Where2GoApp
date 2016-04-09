55a496d69ba3d58e4894ef48173f59347bf4441c
gary <garywjd91@gmail.com>
2016-04-09 22:02:35 +0800

    heroku push13

--- cs4244app/__init__.py
+++ cs4244app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 from cs4244app.expertapp.controller import expertapp
 import os
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+
 
 
 app.register_blueprint(expertapp, url_prefix='/expertapp')
