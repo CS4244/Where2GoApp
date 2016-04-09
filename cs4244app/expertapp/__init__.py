3fba8d2e34e9e08a9e9aed3be6aa6767c14e07ca
gary <garywjd91@gmail.com>
2016-04-09 22:55:17 +0800

    heroku push131

--- cs4244app/expertapp/__init__.py
+++ cs4244app/expertapp/__init__.py
@@ -3,6 +3,6 @@ import clips, os
 currentPath = os.path.dirname(os.path.realpath(__file__))
 # clips.Load("".join((currentPath, '\CLP\data.cl#p')))
 print(currentPath)
-clips.Load('data.CLP')
+clips.Load('/app/cs4244app/expertapp/data.CLP')
 clips.Reset()
 clips.Run()
