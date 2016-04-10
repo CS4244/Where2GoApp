5ebf825851ed285c5e62e3b38f8e295f76cebeb3
gary <garywjd91@gmail.com>
2016-04-10 10:10:47 +0800

    added session, edited clp

--- cs4244app/expertapp/controller.py
+++ cs4244app/expertapp/controller.py
@@ -1,10 +1,10 @@
-from flask import render_template, url_for, request, redirect, Blueprint
+from flask import render_template, url_for, request, redirect, Blueprint, session
 import clips
 import sys
 import cStringIO
 import os
 
-qnId = 1
+
 
 expertapp = Blueprint('expertapp', __name__, template_folder='templates')
 
@@ -26,11 +26,8 @@ def printFacts():
     return facts
 
 
-def isQuestionTypeRadioInAskFact():
-    questionType = findAskFact().Slots['questionType']
-    if questionType == "RADIO":
-        return True
-    return False
+def getQuestionTypeInAskFact():
+    return findAskFact().Slots['questionType']
 
 
 def getSlotTypeInAskFact():
@@ -60,31 +57,34 @@ def findDesiredFact():
 
 @expertapp.route('/', methods=['GET', 'POST'])  # localhost:5000/expertapp/
 def index():
-    qnId = 1
     clips.Reset()
     clips.Run()
     if request.method == 'POST':
         if request.form['beginbtn'] == "BEGIN":
+            session['start'] = True
             return redirect(url_for('expertapp.question1'))
 
     return render_template('begin.html')
 
+
 @expertapp.route('/end', methods=['GET', 'POST'])  # localhost:5000/expertapp/
 def end():
+    session.pop('start', None)
     factResult = printFacts().split('\n')
     if request.method == 'POST':
         return redirect(url_for('expertapp.index'))
-    return render_template('end.html', factResult = factResult)
+    return render_template('end.html', factResult=factResult)
 
 
 @expertapp.route('/question', methods=['GET', 'POST'])  # localhost:5000/expertapp/question1
 def question1():
-    global qnId
     factResult = printFacts().split('\n')
+    print(len(factResult))
 
     if findAskFact() is None:
         return redirect(url_for('expertapp.end'))
-
+    if not('start' in session):
+        return redirect(url_for('expertapp.index'))
 
     # aaa = findDesiredFact()
     # tempMod1 = '(modify '+str(aaa.Index)+' (englishSpeaking "Yes"))'
@@ -93,10 +93,13 @@ def question1():
     # print(findDesiredFact().Slots['englishSpeaking'])
     # print(aaa.Slots.keys())
     # clips.PrintFacts()
-
     if request.method == 'POST':
-        qnId += 1
         print(request.form)
+        if (len(request.form) == 0):
+            return render_template('question.html', choiceList=getChoiceInAskFact(),
+                                   question=getQuestionInAskFact(),
+                                   slotName=getSlotTypeInAskFact(), questionType=getQuestionTypeInAskFact(),
+                                   factResult=factResult, error = True)
         dictKey = list(request.form.keys())[0]
         combinedSlots = ' '
         valueList = request.form.getlist(dictKey)
@@ -105,82 +108,19 @@ def question1():
         toModify = '(modify ' + str(findDesiredFact().Index) + ' (' + dictKey + combinedSlots + '))'
         clips.SendCommand(toModify)
         findAskFact().Retract()
-        clips.Assert("(check"+dictKey+" on)")
+        clips.Assert("(check" + dictKey + " on)")
         clips.Run()
+        clips.DebugConfig.ActivationsWatched = True
+        clips.DebugConfig.FactsWatched = True
+        t = clips.TraceStream.Read()
+        print t
+        clips.PrintFacts()
         return redirect(url_for('expertapp.question1'))
-    return render_template('question.html', qnId=qnId, choiceList=getChoiceInAskFact(), question=getQuestionInAskFact(),
-                           slotName=getSlotTypeInAskFact(), questionType=isQuestionTypeRadioInAskFact(),
+    return render_template('question.html', choiceList=getChoiceInAskFact(), question=getQuestionInAskFact(),
+                           slotName=getSlotTypeInAskFact(), questionType=getQuestionTypeInAskFact(),
                            factResult=factResult)
 
 
-@expertapp.route('/question2', methods=['GET', 'POST'])  # localhost:5000/expertapp/question2
-def question2():
-    qnId = 2
-    clips.PrintFacts()
-    if request.method == 'POST':
-        climate = request.form['climate']
-        fact = ''.join(["(attribute (name climate) (value ", climate, "))"])
-        clips.Assert(fact)
-        clips.PrintFacts()
-        return redirect(url_for('expertapp.question3'))
-
-    return render_template('question2.html', qnId=qnId)
-
-
-@expertapp.route('/question3', methods=['GET', 'POST'])  # localhost:5000/expertapp/question3
-def question3():
-    qnId = 3
-    clips.PrintFacts()
-    if request.method == 'POST':
-        englishSpeaker = request.form['english']
-        fact = ''.join(["(attribute (name english) (value ", englishSpeaker, "))"])
-        clips.Assert(fact)
-        clips.PrintFacts()
-        return redirect(url_for('expertapp.question4'))
-
-    return render_template('question3.html', qnId=qnId)
-
-
-@expertapp.route('/question4', methods=['GET', 'POST'])  # localhost:5000/expertapp/question4
-def question4():
-    qnId = 4  # set the question number in the html
-    clips.PrintFacts()
-    if request.method == 'POST':
-        visit = request.form['visit']
-        fact = ''.join(["(attribute (name visit) (value ", visit, "))"])
-        clips.Assert(fact)
-        clips.PrintFacts()
-        return redirect(url_for('expertapp.question5'))
-
-    return render_template('question4.html', qnId=qnId)
-
-
-@expertapp.route('/question5', methods=['GET', 'POST'])  # localhost:5000/expertapp/question4
-def question5():
-    qnId = 5  # set the question number in the html
-    clips.PrintFacts()
-    if request.method == 'POST':
-        indoorActivity = request.form['indoorActivity']
-        fact = ''.join(["(attribute (name indoorActivity) (value ", indoorActivity, "))"])
-        clips.Assert(fact)
-        clips.PrintFacts()
-        return redirect(url_for('expertapp.question6'))
-
-    return render_template('question5.html', qnId=qnId)
-
-
-@expertapp.route('/question6', methods=['GET', 'POST'])  # localhost:5000/expertapp/question4
-def question6():
-    qnId = 6  # set the question number in the html
-    clips.PrintFacts()
-    if request.method == 'POST':
-        outdoorActivity = request.form['outdoorActivity']
-        fact = ''.join(["(attribute (name outdoorActivity) (value ", outdoorActivity, "))"])
-        clips.Assert(fact)
-        clips.PrintFacts()
-        return redirect(url_for('expertapp.question7'))
-
-    return render_template('question6.html', qnId=qnId)
 
     #
     # @expertapp.route('/question3', methods=['GET', 'POST'])  # localhost:5000/expertapp/question1
